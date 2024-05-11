0d35fb64325b059c89eb198247c294224e86a4b8
Suchit G <suchitg04@gmail.com>
2024-05-11 19:21:09 +0530

    Remove appDir and add fallback page

--- svelte.config.js
+++ svelte.config.js
@@ -6,9 +6,8 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		appDir: "app",
 		adapter: adapter({
-			strict: false
+			fallback: "404.html"
 		}),
 	},
 	preprocess: vitePreprocess()
