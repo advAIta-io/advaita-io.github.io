d52ceb583bb2ac5aa15f08dd3fac8b6d6ddae078
Suchit G <suchitg04@gmail.com>
2024-05-17 14:26:33 +0530

    Updates

---
+++ _app/immutable/chunks/paths.ByGcIfCi.js
@@ -0,0 +1,1 @@
+var s;const t=((s=globalThis.__sveltekit_1lnuj1u)==null?void 0:s.base)??"/advAIta-io";var a;const e=((a=globalThis.__sveltekit_1lnuj1u)==null?void 0:a.assets)??t;export{e as a,t as b};
---
+++ _app/immutable/entry/start.py9JSOoc.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.Crvq4IY7.js";export{t as start};
